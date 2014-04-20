a5be9fb6c08de4f2db47a8b3e4125d3ea75e6a48
Raymond Wang <minjianwang@gmail.com>
2014-04-19 23:40:02 -0700

    Initial Commit

---
+++ Homepwner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Homepwner
+//
+//  Created by Minjian Wang on 4/19/14.
+//  Copyright (c) 2014 big nerd ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
